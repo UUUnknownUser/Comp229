791da6a7bc2e3dba98c94a18be5c053e453223f8
UUUnknownUser <113260742+UUUnknownUser@users.noreply.github.com>
2022-10-22 15:42:51 -0400

    message

---
+++ Mid term/COMP229-F2022-MidTerm-301230390/server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI":'mongodb+srv://henry:OzBzSt3OK01U0RX7@cluster0.lx21k8x.mongodb.net/mean-book?retryWrites=true&w=majority'
+};
