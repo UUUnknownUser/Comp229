6f740a15229d9a8e6ffb4cb90c9948c136a88e3e
UUUnknownUser <113260742+UUUnknownUser@users.noreply.github.com>
2022-10-25 16:54:42 -0400

    revised

--- COMP229Assignment2/config/env/development.js
+++ COMP229Assignment2/config/env/development.js
@@ -3,5 +3,6 @@
 module.exports={
 
     sessionSecret:'developmentSessionSecret',
-    db: 'mongodb://localhost/mean-book'
+    //db: 'mongodb://localhost/mean-book',
+    db: 'mongodb+srv://henry:OzBzSt3OK01U0RX7@cluster0.lx21k8x.mongodb.net/mean-book?retryWrites=true&w=majority'
 };
